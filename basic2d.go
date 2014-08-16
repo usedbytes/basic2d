abecef66ef15c5faa7d32ccf08e7f8c77e99b5cd
Brian Starkey <stark3y@gmail.com>
2014-08-16 12:17:41 +0100

    Add copyright header and COPYING

--- basic2d.go
+++ basic2d.go
@@ -1,3 +1,22 @@
+/*
+ *
+ * basic2d.go - A library for 2D primitives using image/draw
+ *   Copyright Brian Starkey 2013-2014 <stark3y@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of version 2 of the GNU General Public License as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package basic2d
 
 import (
@@ -15,7 +34,7 @@ func Box(dst draw.Image, r image.Rectangle, w int, c color.Color) {
 
     // Opaque mask
     draw.Draw(mask, mask.Bounds(), &image.Uniform{black}, image.ZP, draw.Src)
-    // Transparent rectangle full size of box    
+    // Transparent rectangle full size of box
     draw.Draw(mask, r, &image.Uniform{color.Alpha{255}}, image.ZP, draw.Src)
     // Opaque rectangle inside box (leaving only border transparent)
     ir := image.Rectangle{r.Min.Add(image.Point{w, w}), r.Max.Sub(image.Point{w, w})}
@@ -24,5 +43,5 @@ func Box(dst draw.Image, r image.Rectangle, w int, c color.Color) {
     }
     draw.Draw(mask, ir, &image.Uniform{black}, image.ZP, draw.Src)
 
-    draw.DrawMask(dst, dst.Bounds(), &image.Uniform{c}, image.ZP, mask, image.ZP, draw.Over) 
+    draw.DrawMask(dst, dst.Bounds(), &image.Uniform{c}, image.ZP, mask, image.ZP, draw.Over)
 }
